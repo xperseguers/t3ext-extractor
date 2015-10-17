ae54cfb056201ee3d5391614b57a5ba965bef9ab
Xavier Perseguers <xavier@typo3.org>
2015-10-17 11:03:20 +0200

    [TASK] Rework purpose of this extension
    Change-Id: I51f6bea298faf1d8ae64e815a9ec8a4c80892d1b
    Reviewed-on: http://review.typo3.org/44122
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "extractor".
  *
- * Auto generated 15-03-2014 15:08
+ * Auto generated 17-10-2015 11:00
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -11,38 +11,35 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Bridge between Metadata Services and FAL Extractors',
-	'description' => 'Provides a bridge between TYPO3 metadata extraction services and FAL extractors. Lets you use extensions such as "svmetaextract" or "tika" with FAL. Will automatically populate EXIF / IPTC / XMP and other file metadata extracted from images, videos, sounds or standard files (PDF, DOC, ...) by running the FAL indexer scheduler task. NOTE: a configuration option lets you extract metadata on-the-fly while uploading files.',
-	'category' => 'services',
-	'author' => 'Xavier Perseguers (Causal)',
-	'author_company' => 'Causal Sàrl',
-	'author_email' => 'xavier@causal.ch',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '1.0.0-dev',
-	'constraints' => array(
-		'depends' => array(
-			'php' => '5.3.3-5.5.99',
-			'typo3' => '6.2.0-6.2.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => '',
-	'suggests' => array(
-	),
+    'title' => 'Metadata and content analysis service for TYPO3 FAL',
+    'description' => 'This extension detects and extracts metadata and text from potentially thousand different file types (such as MS Word/Powerpoint/Excel documents and PDF) and bring them automatically and natively to TYPO3 when uploading assets.',
+    'category' => 'services',
+    'author' => 'Xavier Perseguers (Causal)',
+    'author_company' => 'Causal Sàrl',
+    'author_email' => 'xavier@causal.ch',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '1.0.0-dev',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '5.3.3-5.6.99',
+            'typo3' => '6.2.0-7.99.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => '',
+    'suggests' => array(
+        'filemetadata' => '',
+    ),
 );
-
-?>
