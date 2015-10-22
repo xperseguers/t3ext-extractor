d45c8ba707300e4bfd3822e20542363ba1baf6ee
Xavier Perseguers <xavier@typo3.org>
2015-10-22 16:48:11 +0200

    [TASK] Backport TCA streamline from TYPO3 v7
    Change-Id: I68bed44cd57c4c7b47481fd435b6e8f8d4363da0
    Resolves: #70952
    Reviewed-on: https://review.typo3.org/44227
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,11 +32,10 @@ $EM_CONF[$_EXTKEY] = array(
         'depends' => array(
             'php' => '5.3.3-5.6.99',
             'typo3' => '6.2.0-7.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(
             'filemetadata' => '',
         ),
+        'conflicts' => array(),
+        'suggests' => array(),
     ),
     '_md5_values_when_last_written' => '',
 );
