eff1ab126d2e5ff099954e3b5a3b8793b52bd72f
Xavier Perseguers <xavier@typo3.org>
2016-01-15 09:37:41 +0100

    [TASK] Cleanup ext_localconf.php and ext_tables.php
    Change-Id: I06cf8704e34482380558dae24606364a1553c614
    Reviewed-on: https://review.typo3.org/45915
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,32 +1,34 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$extractorRegistry = \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance();
+$boot = function ($_EXTKEY) {
+    $extractorRegistry = \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance();
 
-$settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-if (is_array($settings)) {
-    if (isset($settings['enable_tika']) && (bool)$settings['enable_tika']) {
-        $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\TikaMetadataExtraction');
-        $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\TikaLanguageDetector');
+    $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+    if (is_array($settings)) {
+        if (isset($settings['enable_tika']) && (bool)$settings['enable_tika']) {
+            $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\TikaMetadataExtraction');
+            $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\TikaLanguageDetector');
+        }
+        // Backward compatibility with removed combined option
+        $externalTools = !isset($settings['enable_tools_exiftool']) && isset($settings['enable_tools']) && (bool)$settings['enable_tools'];
+        if ($externalTools || (isset($settings['enable_tools_exiftool']) && (bool)$settings['enable_tools_exiftool'])) {
+            $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\ExifToolMetadataExtraction');
+        }
+        if ($externalTools || (isset($settings['enable_tools_pdfinfo']) && (bool)$settings['enable_tools_pdfinfo'])) {
+            $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\PdfinfoMetadataExtraction');
+        }
+        // Mind the "!isset" in test below to be backward compatible
+        if (!isset($settings['enable_php']) || (bool)$settings['enable_php']) {
+            $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\PhpMetadataExtraction');
+        }
     }
-    // Backward compatibility with removed combined option
-    $externalTools = !isset($settings['enable_tools_exiftool']) && isset($settings['enable_tools']) && (bool)$settings['enable_tools'];
-    if ($externalTools || (isset($settings['enable_tools_exiftool']) && (bool)$settings['enable_tools_exiftool'])) {
-        $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\ExifToolMetadataExtraction');
-    }
-    if ($externalTools || (isset($settings['enable_tools_pdfinfo']) && (bool)$settings['enable_tools_pdfinfo'])) {
-        $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\PdfinfoMetadataExtraction');
-    }
-    // Mind the "!isset" in test below to be backward compatible
-    if (!isset($settings['enable_php']) || (bool)$settings['enable_php']) {
-        $extractorRegistry->registerExtractionService('Causal\\Extractor\\Service\\Extraction\\PhpMetadataExtraction');
-    }
-}
 
-if (version_compare(TYPO3_version, '7.5.0', '<') && isset($settings['auto_extract']) && (bool)$settings['auto_extract']) {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][] = 'Causal\\Extractor\\Hook\\FileUploadHook';
-}
+    if (version_compare(TYPO3_version, '7.5', '<') && isset($settings['auto_extract']) && (bool)$settings['auto_extract']) {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_extfilefunc.php']['processData'][] = 'Causal\\Extractor\\Hook\\FileUploadHook';
+    }
 
-// Cleanup
-unset($settings);
-unset($extractorRegistry);
+};
+
+$boot($_EXTKEY);
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die();
 
 if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['sv']['foo'] = array(
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['sv']['extractor'] = array(
         'title' => 'LLL:EXT:extractor/Resources/Private/Language/locallang_reports.xlf:report_title',
         'description' => 'LLL:EXT:extractor/Resources/Private/Language/locallang_reports.xlf:report_description',
         'icon' => 'EXT:extractor/Resources/Public/Images/tx_sv_report.png',
