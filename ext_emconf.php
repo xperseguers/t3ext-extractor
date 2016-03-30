cf2579e66e07c342ffd0e176daae0af52f5d812c
Xavier Perseguers <xavier@typo3.org>
2016-03-30 18:41:52 +0200

    [TASK] Compatibility with TYPO3 v8
    Change-Id: I271c9783db195d9963b6c0dde3437ec5f00eabaf
    Reviewed-on: https://review.typo3.org/47433
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.5.0-7.0.99',
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.99.99',
             'filemetadata' => '',
         ),
         'conflicts' => array(),
