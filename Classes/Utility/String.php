53dd4d8b80d2171e8ab463353b43de16338f387f
Xavier Perseguers <xavier@typo3.org>
2016-07-13 07:46:38 +0200

    [BUGFIX] Remove or trim FileSource from JSON
    FileSource indicates the image source. If a DSC recorded the image, this
    tag value is always set to 3, indicating the image was recorded on a DSC.
    
        Tag     =    41728  (A300.H)
        Type    =    UNDEFINED
        Count   =    1
        Default =    3
          3     = DSC
          Other = reserved
    
    This patch introduces a new processor Causal\\Extractor\\Utility\\String::trim
    which trims but removes ASCII control characters as well.
    
    Change-Id: I419032180694a53bc57609413f18b77b510da1bc
    Resolves: #76269
    Reviewed-on: https://review.typo3.org/48951
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

---
+++ Classes/Utility/String.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Causal\Extractor\Utility;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * String utility class.
+ *
+ * @author      Xavier Perseguers <xavier@causal.ch>
+ * @license     http://www.gnu.org/copyleft/gpl.html
+ */
+class String
+{
+
+    /**
+     * Trims a string (and also removes non-printable binary characters).
+     *
+     * @param string $str
+     * @return int
+     */
+    public static function trim($str)
+    {
+        // Remove non-printable characters (ASCII 0-31)
+        $str = preg_replace('/[\x00-\x1F]/', '', $str);
+        return trim($str);
+    }
+
+}
