b2803c004c3e0c9c8a1f0a02ce854d78d16ad7d9
Xavier Perseguers <xavier@causal.ch>
2018-10-24 09:54:28 +0200

    [!!!][TASK] Raise version to 1.7.0-dev
    This drops compatibility with TYPO3 v7.

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,11 +27,11 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '1.6.1-dev',
+    'version' => '1.7.0-dev',
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.2.99',
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
             'filemetadata' => '',
         ],
         'conflicts' => [],
