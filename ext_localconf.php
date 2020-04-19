0cff02d40ea7b3d5244adbaa03fd106c054d2c8e
Xavier Perseguers <xavier@causal.ch>
2020-04-19 19:02:24 +0200

    [BUGFIX] Fix registration of the extractor service in TYPO3 v10

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,11 @@ defined('TYPO3_MODE') || die();
 $boot = function (string $_EXTKEY): void {
     $extractorRegistry = \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance();
 
-    $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+    if (version_compare(TYPO3_version, '9.0', '<')) {
+        $settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY] ?? '') ?? [];
+    } else {
+        $settings = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS'][$_EXTKEY] ?? [];
+    }
     if (is_array($settings)) {
         if (isset($settings['enable_tika']) && (bool)$settings['enable_tika']) {
             $extractorRegistry->registerExtractionService(\Causal\Extractor\Service\Extraction\TikaMetadataExtraction::class);
--- ext_tables.php
+++ ext_tables.php
@@ -1,11 +1,16 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['sv']['extractor'] = [
-        'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_reports.xlf:report_title',
-        'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_reports.xlf:report_description',
-        'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/tx_sv_report.png',
-        'report' => \Causal\Extractor\Report\ServicesListReport::class
-    ];
-}
+$boot = function (string $_EXTKEY): void {
+    if (TYPO3_MODE === 'BE') {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['sv']['extractor'] = [
+            'title' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_reports.xlf:report_title',
+            'description' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_reports.xlf:report_description',
+            'icon' => 'EXT:' . $_EXTKEY . '/Resources/Public/Images/tx_sv_report.png',
+            'report' => \Causal\Extractor\Report\ServicesListReport::class
+        ];
+    }
+};
+
+$boot('extractor');
+unset($boot);
