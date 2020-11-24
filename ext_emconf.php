d50b2fe8a381f6d9fe3ad01f429caf772c801e79
Xavier Perseguers <xavier@causal.ch>
2020-11-24 06:32:01 +0100

    [TASK] Remove deprecated and removed $EM_CONF options

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,16 +17,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Xavier Perseguers',
     'author_company' => 'Causal Sàrl',
     'author_email' => 'xavier@causal.ch',
-    'shy' => '',
-    'priority' => '',
-    'module' => '',
     'state' => 'stable',
-    'internal' => '',
     'uploadfolder' => 0,
     'createDirs' => '',
-    'modify_tables' => '',
     'clearCacheOnLoad' => 0,
-    'lockType' => '',
     'version' => '1.7.3',
     'constraints' => [
         'depends' => [
@@ -37,5 +31,4 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [],
         'suggests' => [],
     ],
-    '_md5_values_when_last_written' => '',
 ];
