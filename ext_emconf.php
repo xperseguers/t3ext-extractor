fa58230c6886ffc73dc6ac296255417e5f9c939c
Xavier Perseguers <xavier@causal.ch>
2023-11-07 14:48:09 +0100

    [TASK] Kickstart compatibility with TYPO3 v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-8.2.99',
-            'typo3' => '8.7.0-11.5.99',
+            'typo3' => '8.7.0-12.4.99',
             'filemetadata' => '',
         ],
         'conflicts' => [],
