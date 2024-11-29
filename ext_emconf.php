26c039349a175113fbf38b0f59df9616b6d18064
Xavier Perseguers <xavier@causal.ch>
2024-11-29 12:16:47 +0100

    [TASK] Show that we want to support PHP 8.4 as well

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.5.1',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-8.3.99',
+            'php' => '7.2.0-8.4.99',
             'typo3' => '8.7.0-13.4.99',
             'filemetadata' => '',
         ],
